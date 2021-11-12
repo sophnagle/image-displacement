0d0ebfa25d1749bca06b8506963deb2103611407
sophnagle <sophnagle@gmail.com>
2021-11-12 14:35:20 +0000

    Add pixi app

--- assets/js/imagedisplacement.js
+++ assets/js/imagedisplacement.js
@@ -8,4 +8,16 @@ sections.forEach(section => {
     const originalImageSource = originalImage.getAttribute("src")
     // remove contents of section
     section.innerHTML = ""
+
+    // set up a pixi application 
+    const app = new PIXI.Application({
+        // these can be editied depending on the size of the images selected
+        width: 1100,
+        height: 800,
+        transparent: true
+    })
+
+    // add the pixi application to the section tag
+    section.appendChild(app.view)
+
 })
