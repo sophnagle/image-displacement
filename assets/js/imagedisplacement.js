b627ff1d6762ab1caf86184a745e714cf1ab0a7a
sophnagle <sophnagle@gmail.com>
2021-11-12 14:50:53 +0000

    Import image into PIXI

--- assets/js/imagedisplacement.js
+++ assets/js/imagedisplacement.js
@@ -20,4 +20,27 @@ sections.forEach(section => {
     // add the pixi application to the section tag
     section.appendChild(app.view)
 
+    // make a new image
+    let image = null
+
+    // make a new loader
+    const loader = new PIXI.loaders.Loader()
+
+    // load in our image 
+    loader.add("image", originalImageSource)
+    loader.load((loader, resources) => {
+        // once the image had loaded now do things
+        image = new PIXI.Sprite(resources.image.texture)
+
+        image.x = 100
+        image.y = 100
+        image.width = 900
+        image.height = 600
+        // this makes the image interactive
+        image.interactive = true
+
+        // add the image to the app
+        app.stage.addChild(image)
+    })
+
 })
